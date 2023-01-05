fe40491d57128376ede59c05d6a5e95eaf9097a5
rohit <ramteker284@gmail.com>
2023-01-05 10:51:23 +0530

    ok

---
+++ c6.php
@@ -0,0 +1,53 @@
+<?php
+
+class Student
+{
+    public $roll ;
+    public $name ;
+    public $branch ;
+    public $year ;
+
+    function setdata($roll , $name  ,$branch , $year)
+    {
+        $this->roll = $roll ;
+        $this->name = $name ;
+        $this->branch = $branch ;
+        $this->year = $year ;
+
+        /* ....for console input
+        
+        $this ->roll = (int)readline(' Prn : ') ;
+        $this ->name = readline(' Name : ') ;
+        $this ->branch = readline(' Branch : ') ;
+        $this ->year = (int)readline(' Year : ') ;
+
+        .....just use obj->serdata() ; ..at end now
+        */
+    }
+
+    function getdata($index)
+    {
+        echo "<b>Student ".$index ." : </b><br><br>" ;
+        echo "PRN : ".$this->roll."<br>" ;
+        echo "Name : ".$this->name."<br>" ;
+        echo "Branch : ".$this->branch."<br>" ;
+        echo "Year : ".$this->year."<br><br><hr>" ;
+
+    }
+
+}
+
+        $s1 = new Student() ;
+        $s2 = new Student() ;
+        $s3 = new Student() ;
+
+        echo "<h2>Student Info :</h2><br><hr>" ;
+        $s1 -> setdata(1 , "Arun" , "Computer" , 1) ;
+        $s2 -> setdata(2 , "Binod" , "ENTC" , 2) ;
+        $s3 -> setdata(3 , "Chinmay" , "Civil" , 3) ;
+
+        $s1 -> getdata(1) ;
+        $s2 -> getdata(2) ;
+        $s3 -> getdata(3) ;
+
+?>
